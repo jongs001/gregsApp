bbbcbe4a3865f9f8d9b3b5f0b2a3fbd37ec50898
jongs001 <jongs001@mymail.unisa.edu.au>
2022-09-17 08:02:22 +0930

    add customer auth

--- src/customer.js
+++ src/customer.js
@@ -9,7 +9,9 @@ function Customer(){
             <main role="main" class="inner cover">
                 <h1 class="cover-heading">Welcome to the Customer's Profile Page</h1>
                 <p class="lead">Under Construction!</p>
-             
+                <a class="btn btn-primary mr-1" href="https://nuber-auth.auth.us-east-1.amazoncognito.com/logout?client_id=47igshm19nqog74vhqf3dkglhd&logout_uri=https://main.dlhg8804ua3jk.amplifyapp.com/">
+                    Sign out
+                </a>  
             </main>
 
             
--- src/login.js
+++ src/login.js
@@ -9,7 +9,7 @@ function Login(){
             <main role="main" class="inner cover">
             <h3 class="cover-heading">Please Select Account Type:</h3>
             <div class="cover-container d-flex h-100 p-3 flex-column  btn-group justify-content-center mx-auto btn-block m-2 span2" >
-                <a  class="btn-lg btn-secondary btn-block m-1"   href="https://nuber-auth.auth.us-east-1.amazoncognito.com/login?client_id=47igshm19nqog74vhqf3dkglhd&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://dev8880.d1okzpojss46dn.amplifyapp.com/customer_page">Customer</a>
+                <a  class="btn-lg btn-secondary btn-block m-1"   href="https://nuber-auth.auth.us-east-1.amazoncognito.com/login?client_id=7pc6eokqc4kk06k243cnfc8964&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://main.dlhg8804ua3jk.amplifyapp.com/customer">Customer</a>
                 <Link  class="btn-lg btn-secondary  btn-block m-1"   to="/drivers">Driver</Link>
             </div>
             </main>
